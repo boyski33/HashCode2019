3971ea9cceb1832cbb75a322da9435c3c33a67d5
vpenelski <victor.penelski@gmail.com>
2019-02-28 23:01:18 +0200

    naive pet pictures

---
+++ hashcode/src/main/java/com/github/NaivePetPictures.java
@@ -0,0 +1,96 @@
+package com.github;
+
+import com.github.io.InputDataSetReader;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static java.util.Collections.disjoint;
+
+public class NaivePetPictures implements DataSet {
+
+    private static final int ATTEMPTS = 5_000_000;
+
+    @Override
+    public List<String> run() throws IOException, URISyntaxException {
+        InputDataSetReader reader = new InputDataSetReader();
+
+        List<Picture> pictures = reader.read("d_pet_pictures.txt");
+
+        List<String> result = new ArrayList<>();
+
+        int iterrationsLeft = ATTEMPTS;
+        while (iterrationsLeft-- > 0 && pictures.size() > 3) {
+
+            PicturesForSlide first = getSlide(pictures);
+            PicturesForSlide second = getSlide(pictures);
+
+            if (!disjoint(first.slide.tags, second.slide.tags)) {
+                result.add(String.valueOf(first.slide.printableIds()));
+                result.add(String.valueOf(second.slide.printableIds()));
+            } else {
+                pictures.addAll(first.picturesUsed);
+                pictures.addAll(second.picturesUsed);
+            }
+
+        }
+
+        for (int i = 0; i < pictures.size(); i++) {
+            if (i == pictures.size() - 1) {
+                result.add(Slide.of(pictures.get(i)).printableIds());
+                break;
+            }
+
+            result.add(Slide.of(pictures.get(i++), pictures.get(i)).printableIds());
+        }
+
+        return result;
+
+    }
+
+
+    private PicturesForSlide getSlide(List<Picture> pictures) {
+        Random random = new Random();
+        Picture pic = pictures.remove(random.nextInt(pictures.size()));
+
+        if (pic.orientation == 'H') {
+            return PicturesForSlide.of(Slide.of(pic), List.of(pic));
+        }
+
+        List<Picture> unused = new ArrayList<>();
+        Picture second = null;
+        while (second == null) {
+            second = pictures.remove(random.nextInt(pictures.size()));
+            if (second.orientation == 'V') {
+                pictures.addAll(unused);
+                return PicturesForSlide.of(Slide.of(pic, second), List.of(pic, second));
+            }
+
+            unused.add(second);
+        }
+
+        pictures.addAll(unused);
+        return PicturesForSlide.of(Slide.of(pic), List.of(pic));
+    }
+
+    private static class PicturesForSlide {
+        Slide slide;
+        List<Picture> picturesUsed;
+
+        private PicturesForSlide() {
+        }
+
+        public static PicturesForSlide of(Slide slide, List<Picture> picturesUsed) {
+            PicturesForSlide picturesForSlide = new PicturesForSlide();
+            picturesForSlide.slide = slide;
+            picturesForSlide.picturesUsed = picturesUsed;
+
+            return picturesForSlide;
+        }
+    }
+
+
+}
