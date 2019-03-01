e50de86f46d44d693355eddf79ac8eb3c8c4a264
Boyan Kushlev <takarabukara@gmail.com>
2019-03-01 18:47:55 +0200

    use algorithm interface for memorable moments dataset

--- hashcode/src/main/java/com/github/NaiveMemorableMoments.java
+++ hashcode/src/main/java/com/github/NaiveMemorableMoments.java
@@ -3,6 +3,7 @@ package com.github;
 import static com.github.DataSetUtil.getRandomSlide;
 import static java.util.Collections.disjoint;
 
+import com.github.algorithm.Algorithm;
 import com.github.io.InputDataSetReader;
 
 import java.io.IOException;
@@ -12,40 +13,19 @@ import java.util.List;
 
 public class NaiveMemorableMoments implements DataSet {
 
-    private static final int ATTEMPTS = 15_000_000;
+    private Algorithm algorithm;
+
+    public NaiveMemorableMoments(Algorithm algorithm) {
+        this.algorithm = algorithm;
+    }
 
     @Override
     public InterestResult run() throws IOException, URISyntaxException {
-        InputDataSetReader reader = new InputDataSetReader();
-        int score = 0;
 
+        InputDataSetReader reader = new InputDataSetReader();
         List<Picture> pictures = reader.read("c_memorable_moments.txt");
 
-        List<String> result = new ArrayList<>();
-
-        int iterationsLeft = ATTEMPTS;
-        while (iterationsLeft-- > 0 && pictures.size() > 3) {
-
-            PicturesForSlide first = getRandomSlide(pictures);
-            PicturesForSlide second = getRandomSlide(pictures);
-
-            if (first == null || second == null) {
-                return new InterestResult(score, result);
-            }
-
-            if (!disjoint(first.slide.tags, second.slide.tags)) {
-                result.add(String.valueOf(first.slide.printableIds()));
-                result.add(String.valueOf(second.slide.printableIds()));
-                score += first.slide.interestScore(second.slide);
-
-            } else {
-                pictures.addAll(first.picturesUsed);
-                pictures.addAll(second.picturesUsed);
-            }
-
-        }
-
-        return new InterestResult(score, result);
+        return algorithm.calculateScore(pictures);
     }
 
 }
